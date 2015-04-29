d453e9632978158219aa536804462500f5385cb9
Lucas Fraga Schuler <lucas.f.schuler@hotmail.com>
2015-04-29 14:09:44 -0300

    commit

--- SpotMark/OneEventViewController.m
+++ SpotMark/OneEventViewController.m
@@ -157,6 +157,29 @@
 //        [push setMessage:message2];
 //        [push sendPushInBackground];
         
+        //	PFUser *user = [PFUser currentUser];
+        //	NSString *message = [NSString stringWithFormat:@"%@: %@", user[PF_USER_FULLNAME], text];
+        //
+        //	PFQuery *query = [PFQuery queryWithClassName:PF_RECENT_CLASS_NAME];
+        //	[query whereKey:PF_RECENT_GROUPID equalTo:groupId];
+        //	[query whereKey:PF_RECENT_USER notEqualTo:user];
+        //	[query includeKey:PF_RECENT_USER];
+        //	[query setLimit:1000];
+        //
+        //	PFQuery *queryInstallation = [PFInstallation query];
+        //	[queryInstallation whereKey:PF_INSTALLATION_USER matchesKey:PF_RECENT_USER inQuery:query];
+        //
+        //	PFPush *push = [[PFPush alloc] init];
+        //	[push setQuery:queryInstallation];
+        //	[push setMessage:message];
+        //	[push sendPushInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+        //	{
+        //		if (error != nil)
+        //		{
+        //			NSLog(@"SendPushNotification send error.");
+        //		}
+        //	}];
+        
         [self loadPosts];
     }
 }
