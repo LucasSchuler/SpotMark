5acd3749e6165ba620bfb8fbd7960cf3b2ad6cbf
Lucas Fraga Schuler <lucas.f.schuler@hotmail.com>
2015-05-22 16:33:28 -0300

    o dia ja rendeu

--- SpotMark WatchKit Extension/InterfaceController.swift
+++ SpotMark WatchKit Extension/InterfaceController.swift
@@ -14,41 +14,41 @@ class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var table: WKInterfaceTable!
 
-//    // 1 - Create Rows
-//    let titles = ["Appple Watch Sport", "Apple Watch", "Apple Watch Edition"]
-//    let images = ["watch-0", "watch-1","watch-2"]
+    // 1 - Create Rows
+    let titles = ["Appple Watch Sport", "Apple Watch", "Apple Watch Edition"]
+    let images = ["watch-0", "watch-1","watch-2"]
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-//        // Configure interface objects here.
-//        self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: .AllowAnimatedEmoji) { (results) -> Void in
-//            // Do something here...
-//        }
-//        
-//        
-//        // 2 - Set the number of rows in table
-//        table.setNumberOfRows(titles.count, withRowType: "WatchRow")
-//        
-//        // 3 - Create the rows
-//        for i in 0..<titles.count {
-//            if let row = table.rowControllerAtIndex(i) as? WatchRow {
-//                row.titleLabel.setText(titles[i])
-//            }
-//        }
+        // Configure interface objects here.
+        self.presentTextInputControllerWithSuggestions(nil, allowedInputMode: .AllowAnimatedEmoji) { (results) -> Void in
+            // Do something here...
+        }
+        
+        
+        // 2 - Set the number of rows in table
+        table.setNumberOfRows(titles.count, withRowType: "WatchRow")
+        
+        // 3 - Create the rows
+        for i in 0..<titles.count {
+            if let row = table.rowControllerAtIndex(i) as? WatchRow {
+               row.titleLabel.setText(titles[i])
+            }
+        }
        
     }
 
   
-//    override func contextForSegueWithIdentifier(segueIdentifier: String, inTable table: WKInterfaceTable, rowIndex: Int) -> AnyObject? {
-//        
-//        if segueIdentifier == "gotoDetail" {
-//            let imgWatch = images[rowIndex]
-//            return imgWatch
-//        }
-//        
-//        return nil
-//    }
+    override func contextForSegueWithIdentifier(segueIdentifier: String, inTable table: WKInterfaceTable, rowIndex: Int) -> AnyObject? {
+        
+        if segueIdentifier == "gotoDetail" {
+            let imgWatch = images[rowIndex]
+            return imgWatch
+        }
+        
+        return nil
+    }
 
     
     override func willActivate() {
---
+++ SpotMark WatchKit Extension/WatchRow.swift
@@ -0,0 +1,15 @@
+//
+//  WatchRow.swift
+//  SpotMark
+//
+//  Created by Lucas Fraga Schuler on 22/05/15.
+//  Copyright (c) 2015 Lucas Fraga Schuler. All rights reserved.
+//
+
+import WatchKit
+
+class WatchRow: NSObject {
+   
+    @IBOutlet weak var titleLabel: WKInterfaceLabel!
+
+}
