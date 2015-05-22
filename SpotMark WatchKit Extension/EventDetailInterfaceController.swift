07ae6e4dfd36d60f7ef7e805b0982e1456a4e754
Rafaela S Bertolini <bertolinifr@gmail.com>
2015-05-22 16:08:57 -0300

    aloha

---
+++ SpotMark WatchKit Extension/EventDetailInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  EventDetailInterfaceController.swift
+//  SpotMark
+//
+//  Created by Rafaela dos Santos Bertolini on 22/05/15.
+//  Copyright (c) 2015 Lucas Fraga Schuler. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class EventDetailInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
