b24df6d63647762de0fbe975219996b9bfa52bfa
Lucas Fraga Schuler <lucas.f.schuler@hotmail.com>
2015-05-22 13:37:47 -0300

    apple watch target

---
+++ SpotMark WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SpotMark WatchKit Extension
+//
+//  Created by Lucas Fraga Schuler on 22/05/15.
+//  Copyright (c) 2015 Lucas Fraga Schuler. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
